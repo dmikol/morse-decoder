d2757680abab8929f1c840d6f6178794f909416a
DmiKol <dmikol13@mail.ru>
2020-03-03 11:33:59 +0300

    feat: add solution morse-decoder

--- src/index.js
+++ src/index.js
@@ -38,7 +38,42 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let arrWords = expr.split('**********').map(function(item){
+        let letter = [];
+        let stack = [];
+        for(let i = 1; i <= item.length; i++){
+            stack.push(item[i-1]);
+            if(i != 0 && i % 10 == 0){
+                let str = stack.join('');
+                stack = [];
+                let index = str.indexOf('1');
+                letter.push(str.slice(index));
+            }
+        }
+        return letter;
+    });
+    
+    let arr = arrWords.map(function(item){
+        
+        let resultLetter = [];
+        
+        for(let i = 0; i < item.length; i++){
+            let k = 0;
+            let arrMorze = [];
+            while(k < item[i].length){
+                if(item[i][k]+item[i][k+1] == '10'){
+                    arrMorze.push('.');
+                }else{
+                    arrMorze.push('-');
+                }
+                k += 2;
+            }
+            resultLetter.push(MORSE_TABLE[arrMorze.join('')]);
+        }
+        return resultLetter.join('');
+    })
+
+return arr.join(' ');
 }
 
 module.exports = {
